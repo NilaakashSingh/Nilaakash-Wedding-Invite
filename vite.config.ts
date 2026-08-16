2b9e3dc45df0fa97f4d3d175f582ba7d469133fa
NilaakashSingh <singhnilaakash@gmail.com>
2026-08-15 23:42:22 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Nilaakash-Wedding_Invite/',
+  base: '/Nilaakash-Wedding-Invite/',
   plugins: [react()],
   resolve: {
     alias: {
